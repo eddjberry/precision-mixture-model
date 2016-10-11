7371185fcd8e98073f62e860e4267482929e9e3d
Ed Berry <Ed Berry>
2016-10-11 14:07:29 +0100

    Input as radians

--- test_analysis.R
+++ test_analysis.R
@@ -5,6 +5,9 @@ source("mixture_model_functions.R")
 
 d <- read.csv("test_data.csv")
 
+d$response_rad <- wrap(d$response/90*pi)
+d$target_rad <- wrap(d$target/90*pi)
+
 d1 <- d %>%
   filter(id == "pilot1")
 
@@ -12,14 +15,11 @@ d2 <- d %>%
   filter(id == "pilot2")
 
 
-(ests1 = JV10_fit(X = d1$response, Tg  = d1$target))
+(ests1 = JV10_fit(X = d1$response_rad , Tg  = d1$target_rad))
 
-(ests2 = JV10_fit(X = d2$response, Tg  = d2$target))
-
-# For id == "pilot1": K = 0.1386, Pt = 0.9853, Pn = 0, Pu = 0.0147, LL = -164.9913
-# For id == "pilot2": K = 0.5433, Pt = 0.1003, Pn = 0, Pu = 0.8997, LL = -165.3788
+(ests2 = JV10_fit(X = d2$response_rad , Tg  = d2$target_rad))
 
 # looping over df; the tar.var argument actually defaults to "target" and
 # res.var defaults to response but I've included them here for ease of reading.
 
-JV10_df(d, tar.var = "target", res.var = "response")
+JV10_df(d, tar.var = "target_rad", res.var = "response_rad")
